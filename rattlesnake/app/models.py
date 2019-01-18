3e574ea302a5085d6102513a344bef64ca125a33
Alvinditya Saputra <alvindityas@gmail.com>
2019-01-18 10:29:02 +0700

    Add student model

--- rattlesnake/app/models.py
+++ rattlesnake/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    identityNumber = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('student_edit', kwargs={'pk': self.pk})
