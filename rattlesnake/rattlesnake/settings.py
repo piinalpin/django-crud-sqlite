8dc50c8de9bf9629662ef89e1fe48623fca19b04
Alvinditya Saputra <alvindityas@gmail.com>
2019-02-02 04:40:51 +0700

    change again

--- rattlesnake/rattlesnake/settings.py
+++ rattlesnake/rattlesnake/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+django_heroku.settings(locals())
