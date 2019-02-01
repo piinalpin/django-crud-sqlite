2cb97ec3257010eea738cca2579ca7f773864406
Alvinditya Saputra <alvindityas@gmail.com>
2019-02-02 03:47:20 +0700

    Change static in settings py

--- rattlesnake/rattlesnake/settings.py
+++ rattlesnake/rattlesnake/settings.py
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
