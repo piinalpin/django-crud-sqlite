635050ec27e77c51f435fdfcd1e9be0e869366a5
Alvinditya Saputra <alvindityas@gmail.com>
2019-02-02 04:50:43 +0700

    again

--- rattlesnake/rattlesnake/wsgi.py
+++ rattlesnake/rattlesnake/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import django.core.handlers.wsgi
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rattlesnake.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = django.core.handlers.wsgi.WSGIHandler()
